93417fdb98faa735a87bdcc7b7364428eec96afd
Anil Gupta <anilgupta.bcs@gmail.com>
2025-01-01 12:21:58 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,4 +1,4 @@
-def call(String clone_url, String branch_name, String message_to_print){
+def clone(String clone_url, String branch_name, String message_to_print){
   git url: "${clone_url}", branch: "${branch_name}"
   echo "${message_to_print}"
 }
