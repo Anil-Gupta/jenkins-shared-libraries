5bdaa62aad2bb0aa8ef0d6d63a672ee3dc8a5c82
Anil Gupta <anilgupta.bcs@gmail.com>
2025-01-01 13:02:48 +0530

    Create docker_build.groovy

---
+++ vars/docker_build.groovy
@@ -0,0 +1,4 @@
+def call(String project_name, String image_tag, String dockerhub_user, String message_to_print){
+  sh "docker build -t ${dockerhub_user}/${project_name}:${image_tag}"
+  echo "${message_to_print}"
+}
